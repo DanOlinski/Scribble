fa72589d4a943b3108bc93f2c40b5b91f87e38b1
DanOlinski <daniel.guterres@hotmail.com>
2025-01-23 09:49:56 -0700

    t

--- WORDPRESS_SITE/wp-content/themes/customtheme-child/sticky-notes-crud/create.php
+++ WORDPRESS_SITE/wp-content/themes/customtheme-child/sticky-notes-crud/create.php
@@ -8,15 +8,14 @@
     var_dump("insert into notes (user_id, note) values('$userid', '$note')");
 
     //check that there is a value typed into the form (select form by name)
-    if( strlen($_POST['new_note']) > 0 ){
+    // if( strlen($_POST['new_note']) > 0 ){
         
         //garb data from html elements by their names
         $new_note = $_POST["new_note"];
         $user_id = get_current_user_id();
         
         //use the information above to create a query to the database and save the new information typed into the form
-        $sql="insert into notes (user_id, note) values('1', 'tt')";
-        // $sql="insert into notes (user_id, note) values('$user_id', '$new_note')";
+        $sql="insert into notes (user_id, note) values('$user_id', '$new_note')";
         
          //send query to db
         $conn->query($sql);
@@ -24,6 +23,6 @@
 
         //close connection with database
         $conn->close();
-    }
+    // }
 
 ?>
