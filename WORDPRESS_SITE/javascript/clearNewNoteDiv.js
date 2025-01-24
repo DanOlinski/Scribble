166d48487450c104257fdb95b8b48574beb3f3d7
DanOlinski <daniel.guterres@hotmail.com>
2025-01-23 21:08:47 -0700

    t

--- WORDPRESS_SITE/javascript/clearNewNoteDiv.js
+++ WORDPRESS_SITE/javascript/clearNewNoteDiv.js
@@ -21,15 +21,6 @@ $(document).ready(function() {
                 dataType: "text",
             })
 
-            //after the data is made available for pho script empty out the html element (some browsers like chrome have caching features making the app think there is always data typed into the form, causing the app to save repeated data into the database when the browser is refreshed)
-            // $('#form').empty();
-
-            //after the html element is emptied out, it needs to be placed in the browser once again for the next data entry to be possible
-            // $('#form').append($(`
-            //     <input id="new_note" class="s-notes-submit" type="submit" name="notes_create" value="✔"> 
-            //     <textarea id="text" class="s-notes-input" name="new_note" required="" value=""></textarea>
-            // `));
-
             //reload the page so that the newly created note can be loaded from the database onto the front end
             location.reload();
         }
