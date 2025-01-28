54200fc5a29d394592e19f66bcc3d4f998587183
DanOlinski <daniel.guterres@hotmail.com>
2025-01-28 16:37:27 -0700

    tt

--- test-new-wpdb/server.js
+++ test-new-wpdb/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const morgan = require('morgan');
-const PORT = process.env.PORT || 8100;
+const PORT = process.env.PORT || 8101;
 const app = express();
 
 app.set('view engine', 'ejs');
